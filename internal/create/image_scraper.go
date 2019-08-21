c7672ff7eb92f76250cde49a411607434810ef8f
Ryan Gonzalez <rymg19@gmail.com>
2019-08-21 16:10:36 -0500

    Refactor web-related code

--- internal/create/image_scraper.go
+++ internal/create/image_scraper.go
@@ -5,87 +5,47 @@
 package create
 
 import (
-	"fmt"
+	"github.com/refi64/nsbox/internal/webutil"
 	"github.com/pkg/errors"
 	"golang.org/x/net/html"
-	"net/http"
 	"net/url"
-	"strings"
 )
 
 func getImagesIndexUrl(version string) *url.URL {
 	var urlString = "https://dl.fedoraproject.org/pub/fedora/linux/releases/" + version +
 					"/Container/x86_64/images/"
 
-	url, err := url.Parse(urlString)
+	indexUrl, err := url.Parse(urlString)
 	if err != nil {
 		panic(errors.Wrap(err, "unexpected url parse failed"))
 	}
 
-	return url
-}
-
-func readIndexHtml(index *url.URL) (*html.Node, error) {
-	resp, err := http.Get(index.String())
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to read container index")
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("unexpected response code %d", resp.StatusCode)
-	}
-
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return doc, nil
+	return indexUrl
 }
 
 // We basically have to scrape the index.html where you can download the container base image
 // in order to find the actual URL to download. :/
 
-func findLatestContainerImageUrlStringInNode(node *html.Node) string {
-	if node.Type == html.ElementNode && node.Data == "a" {
-		for _, attr := range node.Attr {
-			const containerFilenamePrefix = "Fedora-Container-Base-"
-
-			if attr.Namespace == "" && attr.Key == "href" && strings.HasPrefix(attr.Val, containerFilenamePrefix) {
-				return attr.Val
-			}
-		}
-	}
-
-	for child := node.FirstChild; child != nil; child = child.NextSibling {
-		if url := findLatestContainerImageUrlStringInNode(child); url != "" {
-			return url
-		}
-	}
-
-	return ""
-}
-
 func findLatestContainerImageUrlInDocument(doc *html.Node) (*url.URL, error) {
-	urlString := findLatestContainerImageUrlStringInNode(doc)
-	if urlString == "" {
+	scanner, err := webutil.NewHrefMatchScanner(`^Fedora-Container-Base-`)
+	if err != nil {
+		panic(err)
+	}
+
+	imageUrl, err := webutil.ScanDocument(doc, scanner)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse located image url")
+	} else if imageUrl == nil {
 		return nil, errors.New("failed to find latest container image url in index")
 	}
 
-	url, err := url.Parse(urlString)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse located image url")
-	}
-
-	return url, nil
+	return imageUrl.(*url.URL), nil
 }
 
 func scrapeLatestContainerImageUrl(version string) (*url.URL, error) {
 	indexUrl := getImagesIndexUrl(version)
 
-	index, err := readIndexHtml(indexUrl)
+	index, err := webutil.FetchHtmlPage(indexUrl)
 	if err != nil {
 		return nil, err
 	}
