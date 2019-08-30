db51772a4131f4b78da5a50fd0da79cf98feb576
Ryan Gonzalez <rymg19@gmail.com>
2019-08-29 20:29:55 -0500

    Migrate host_paths to use substitute_file

---
+++ build/host_paths.template.go
@@ -0,0 +1,8 @@
+// This file was auto-generated. DO NOT EDIT.
+// Use 'gn args' to change the directories there instead.
+
+package paths
+
+const State = "@STATE_DIR"
+const Libexec = "@LIBEXEC_DIR"
+const Share = "@SHARE_DIR"
