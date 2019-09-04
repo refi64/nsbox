e6c2cbad63c40de25ecb659906924018f4832bd1
Ryan Gonzalez <rymg19@gmail.com>
2019-09-03 20:31:41 -0500

    Refactor Go build files to use a helper module

--- build/python-shim.sh
+++ build/python-shim.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-buildconfig = "//build/BUILDCONFIG.gn"
-script_executable = rebase_path("//build/python-shim.sh")
+# Wraps python3 to avoid generating bytecode files.
+
+exec python3 -B "$@"
