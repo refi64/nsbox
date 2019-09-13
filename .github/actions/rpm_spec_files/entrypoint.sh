0509dccbdc38b720849e06a20c6f0e800f18a28e
Ryan Gonzalez <rymg19@gmail.com>
2019-09-13 14:18:46 -0500

    More actions

--- .github/actions/rpm_spec_files/entrypoint.sh
+++ .github/actions/rpm_spec_files/entrypoint.sh
@@ -16,11 +16,6 @@ fi
 gn gen out --args="${gn_args[*]}"
 ninja -C out rpm/nsbox{,-guest-tools}.spec
 
-cat >askpass.sh <<'EOF'
-echo "$git_token"
-EOF
-chmod +x askpass.sh
-
 git clone https://github%40nsbox.dev@github.com/nsbox-bot/rpm-spec-files -b $git_branch
 cp out/rpm/*.spec rpm-spec-files
 cd rpm-spec-files
@@ -28,5 +23,11 @@ git config user.email 'github@nsbox.dev'
 git config user.name 'nsbox-bot'
 git add .
 git commit -am "Automated push at $(date)"
+
+cat >askpass.sh <<'EOF'
+echo "$git_token"
+EOF
+chmod +x askpass.sh
+
 export git_token
 GIT_ASKPASS=./askpass.sh git push
