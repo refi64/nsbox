d2b5ab9c14803161ddc397007544731df4a87b70
Ryan Gonzalez <rymg19@gmail.com>
2020-03-17 00:31:54 -0500

    Use Algolia docsearch, reformat config.js

--- web/.vuepress/config.js
+++ web/.vuepress/config.js
@@ -1,20 +1,19 @@
 module.exports = {
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Images', link: '/images/' },
-      { text: 'FAQ', link: '/faq/' },
-      { text: 'Source', link: 'https://github.com/refi64/nsbox' },
+      {text: 'Home', link: '/'},
+      {text: 'Guide', link: '/guide/'},
+      {text: 'Images', link: '/images/'},
+      {text: 'FAQ', link: '/faq/'},
+      {text: 'Source', link: 'https://github.com/refi64/nsbox'},
     ],
     sidebar: 'auto',
     smoothScroll: true,
+    algolia: {
+      apiKey: '38206f8f24dcc0d443c475ef4d13fac4',
+      indexName: 'nsbox',
+    },
   },
   title: 'nsbox',
-  plugins: [
-    '@vuepress/google-analytics',
-    {
-      'ga': 'UA-55018880-2'
-    }
-  ],
+  plugins: ['@vuepress/google-analytics', {'ga': 'UA-55018880-2'}],
 }
