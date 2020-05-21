refi64/nsbox

commit 04d0099ada6d4550b3f64d2f0bd73e7119533a5f

Use transient units to enter booted containers
Currently, nsenter is always used to enter any container. However,
this breaks booted containers in bizarre ways, as systemd-logind will
try to move the process between cgroups...but the process was never in
any.

This fixes that by spawning a transient unit inside the container
through its systemd instance. A concept called "doors" is introduced
into the session package in order to faciliate this.

Unfortunately, godbus and go-systemd do not work well wrt passing
file descriptors over the bus. Therefore, systemd-run is directly
called to start the unit, but its status is still gathered via
manually contacting the bus.

Fixes #C2.

internal/daemon/transient.go
@@ -66,6 +66,11 @@ func startNsboxd(systemd *systemd1.Conn, nsboxd string, ct *container.Container,
 			false,
 		),
 		systemd1.Property{
+			// This is needed for safety with use of nsbus, see there for more info.
+			Name:  "PrivateTmp",
+			Value: godbus.MakeVariant(true),
+		},
+		systemd1.Property{
 			Name:  "Environment",
 			Value: godbus.MakeVariant(env),
 		},

internal/nsbus/nsbus.go
@@ -1,0 +1,175 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package nsbus
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"unsafe"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/pkg/errors"
+	"github.com/refi64/go-lxtempdir"
+	"github.com/refi64/nsbox/internal/log"
+	"golang.org/x/sys/unix"
+)
+
+/*
+	nsbus works by entering the mount namespace in a forked subprocess and
+	connecting to the D-Bus socket there. However, godbus's NewConn will not
+	give us a UNIX-style transport if we immediately create a connection from
+	the new fd, thus making passing of file descriptors broken. In order to
+	rectify this, a private (thanks to PrivateTmp=true in transient.go) D-Bus
+	socket is created that just forwards everything to the true socket, and
+	the D-Bus connection is created by dialing into the forwarding socket
+	instead of the true one.
+*/
+
+const busPath = "/run/dbus/system_bus_socket"
+const privateBusPath = "/tmp/nsbox_container_bus_socket"
+
+// Use an error value to propagate the fd up from inside the dialing.
+type dialSuccessError struct {
+	fd uintptr
+}
+
+func (e *dialSuccessError) Error() string {
+	return "dial success"
+}
+
+//go:linkname runtime_BeforeFork syscall.runtime_BeforeFork
+func runtime_BeforeFork()
+
+//go:linkname runtime_AfterFork syscall.runtime_AfterFork
+func runtime_AfterFork()
+
+//go:linkname runtime_AfterForkInChild syscall.runtime_AfterForkInChild
+func runtime_AfterForkInChild()
+
+func forkAndRunChild(nsfd, sock int, sockaddr *unix.RawSockaddrUnix, sockaddrLen uintptr) (uintptr, unix.Errno) {
+	var child uintptr
+	var syserr unix.Errno
+
+	runtime_BeforeFork()
+	child, _, syserr = unix.RawSyscall(unix.SYS_CLONE, uintptr(unix.SIGCHLD), 0, 0)
+	if child != 0 || syserr != 0 {
+		return child, syserr
+	}
+
+	runtime_AfterForkInChild()
+
+	if _, _, syserr = unix.RawSyscall(unix.SYS_SETNS, uintptr(nsfd), uintptr(unix.CLONE_NEWNS), 0); syserr != 0 {
+		unix.Exit(int(syserr))
+	}
+
+	if _, _, syserr = unix.RawSyscall(unix.SYS_CONNECT,
+		uintptr(sock), uintptr(unsafe.Pointer(sockaddr)), sockaddrLen); syserr != 0 {
+		unix.Exit(128 + int(syserr))
+	}
+
+	unix.Exit(0)
+	panic("exit failed?")
+}
+
+func copyForever(dest io.Writer, src io.Reader) {
+	for {
+
+		if _, err := io.Copy(dest, src); err != nil {
+			log.Fatal("Copying D-Bus forwarder connection data:", err)
+		}
+	}
+}
+
+func forwardListenerToSocket(listener net.Listener, sockfile *os.File) {
+	conn, err := listener.Accept()
+	listener.Close()
+	if err != nil {
+		sockfile.Close()
+		log.Fatal("Accepting D-Bus forwarder connection:", err)
+	}
+
+	go copyForever(conn, sockfile)
+	go copyForever(sockfile, conn)
+}
+
+func DialBusInsideNamespace(nspid int) (*dbus.Conn, error) {
+	// We don't worry about closing the directory, as it will be gone safely
+	// the moment that the nsbox service exits anyway.
+
+	forwardSockDir, err := lxtempdir.Create("", "nsbox-socket")
+	if err != nil {
+		return nil, errors.Wrap(err, "craete nsbus socket dir")
+	}
+
+	forwardSockPath := filepath.Join(forwardSockDir.Path, "bus")
+
+	nsfd, err := unix.Open(fmt.Sprintf("/proc/%d/ns/mnt", nspid), unix.O_RDONLY, 0)
+	if err != nil {
+		return nil, errors.Wrap(err, "open mnt ns fd")
+	}
+	defer unix.Close(nsfd)
+
+	var sockfile *os.File
+
+	sock, err := unix.Socket(unix.AF_UNIX, unix.SOCK_STREAM, 0)
+	if err != nil {
+		return nil, errors.Wrap(err, "create socket")
+	}
+	defer func() {
+		if sockfile == nil {
+			unix.Close(sock)
+		}
+	}()
+
+	var sockaddr unix.RawSockaddrUnix
+	sockaddr.Family = unix.AF_UNIX
+	busPathBytes := []byte(busPath)
+	copy(sockaddr.Path[:], *(*[]int8)(unsafe.Pointer(&busPathBytes)))
+	sockaddr.Path[len(busPathBytes)] = 0
+
+	sockaddrLen := unsafe.Sizeof(sockaddr.Family) + uintptr(len(busPathBytes)+1)
+
+	child, syserr := forkAndRunChild(nsfd, sock, &sockaddr, sockaddrLen)
+	runtime_AfterFork()
+
+	if syserr != 0 {
+		return nil, errors.Wrap(syserr, "fork")
+	}
+
+	process, err := os.FindProcess(int(child))
+	if err != nil {
+		log.Fatal("FindProcess returned unexpected error:", err)
+	}
+
+	state, err := process.Wait()
+	if err != nil {
+		return nil, errors.Wrap(err, "wait for bus connector")
+	}
+
+	if !state.Exited() || state.ExitCode() != 0 {
+		return nil, errors.Errorf("bus connector: %v", state)
+	}
+
+	forwardListener, err := net.Listen("unix", forwardSockPath)
+	if err != nil {
+		unix.Close(sock)
+		return nil, errors.Wrap(err, "forward listener")
+	}
+
+	sockfile = os.NewFile(uintptr(sock), "d-bus forward socket")
+
+	bus, err := dbus.Dial("unix:path=" + forwardSockPath)
+	if err != nil {
+		unix.Close(sock)
+		forwardListener.Close()
+		return nil, errors.Wrap(err, "dialing forwarded bus socket")
+	}
+
+	go forwardListenerToSocket(forwardListener, sockfile)
+	return bus, nil
+}

BUILD.gn
@@ -105,6 +105,7 @@ copy("gofiles") {
     "internal/log/log.go",
     "internal/network/firewalld.go",
     "internal/network/network.go",
+    "internal/nsbus/nsbus.go",
     "internal/nspawn/builder.go",
     "internal/paths/paths.go",
     "internal/ptyservice/client.go",
@@ -112,6 +113,8 @@ copy("gofiles") {
     "internal/release/release.go",
     "internal/selinux/selinux.go",
     "internal/session/enter.go",
+    "internal/session/enter_nsenter.go",
+    "internal/session/enter_systemd.go",
     "internal/session/setup.go",
     "internal/userdata/check_privs.go",
     "internal/userdata/userdata.go",

go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/godbus/dbus/v5 v5.0.3
 	github.com/google/go-containerregistry v0.0.0-20200320200342-35f57d7d4930
 	github.com/google/subcommands v1.2.0
+	github.com/google/uuid v1.1.1
 	github.com/opencontainers/selinux v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/refi64/go-lxtempdir v0.0.0-20190815193640-e8f0a4e7825f

go.sum
@@ -134,6 +134,7 @@ github.com/google/pprof v0.0.0-20181206194817-3ea8567a2e57/go.mod h1:zfwlbNMJ+OI
 github.com/google/subcommands v1.2.0 h1:vWQspBTo2nEqTUFita5/KeEWlUL8kQObDFbub/EN9oE=
 github.com/google/subcommands v1.2.0/go.mod h1:ZjhPrFU+Olkh9WazFPsl27BQ4UPiG37m3yTrtFlrHVk=
 github.com/google/uuid v1.0.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
+github.com/google/uuid v1.1.1 h1:Gkbcsh/GbpXz7lPftLA3P6TYMwjCLYm83jiFQZF/3gY=
 github.com/google/uuid v1.1.1/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
 github.com/googleapis/gax-go/v2 v2.0.4/go.mod h1:0Wqv26UfaUD9n4G6kQubkQ+KchISgw+vpHVxEJEs9eg=
 github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d/go.mod h1:sJBsCZ4ayReDTBIg8b9dl28c5xFWyhBTVRp3pOg5EKY=

internal/selinux/selinux.go
@@ -20,31 +20,44 @@ func Enforcing() bool {
 	return selinux.EnforceMode() == selinux.Enforcing
 }
 
-func SetExecProcessContextContainer() error {
-	if selinux.EnforceMode() == selinux.Disabled {
-		log.Debug("SELinux is disabled")
-		return nil
-	}
+func Enabled() bool {
+	return selinux.EnforceMode() != selinux.Disabled
+}
 
+func GetCurrentLabel() (string, error) {
 	label, err := selinux.ExecLabel()
 	if err == io.EOF {
 		label, err = selinux.CurrentLabel()
 	}
 
-	if err != nil {
-		return errors.Wrap(err, "failed to get label")
-	}
+	return label, err
+}
 
-	parts := strings.Split(label, ":")
+func GetExecLabel(currentLabel string) (string, error) {
+	parts := strings.Split(currentLabel, ":")
 	if len(parts) != 4 && len(parts) != 5 {
-		return errors.Errorf("invalid SELinux label: %s")
+		return "", errors.Errorf("invalid SELinux label: %s")
 	}
 
 	parts[1] = systemRole
 	parts[2] = spcType
 
-	newLabel := strings.Join(parts, ":")
-	log.Debug("SELinux exec transition", label, "->", newLabel)
+	return strings.Join(parts, ":"), nil
+}
+
+func SetExecProcessContextContainer() error {
+	if !Enabled() {
+		log.Debug("SELinux is disabled")
+		return nil
+	}
+
+	currentLabel, err := GetCurrentLabel()
+	if err != nil {
+		return errors.Wrap(err, "get current label")
+	}
+
+	newLabel, err := GetExecLabel(currentLabel)
+	log.Debug("SELinux exec transition", currentLabel, "->", newLabel)
 
 	if err := selinux.SetExecLabel(newLabel); err != nil {
 		return errors.Wrap(err, "failed to set label")

internal/session/enter.go
@@ -9,22 +9,55 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"os/signal"
-	"strconv"
-	"syscall"
 
 	"github.com/coreos/go-systemd/v22/machine1"
 	krpty "github.com/creack/pty"
-	"github.com/pkg/errors"
 	"github.com/refi64/nsbox/internal/container"
 	"github.com/refi64/nsbox/internal/log"
 	"github.com/refi64/nsbox/internal/ptyservice"
-	"github.com/refi64/nsbox/internal/selinux"
 	"github.com/refi64/nsbox/internal/userdata"
 	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/sys/unix"
 )
+
+type ptyIoFlags int
+
+const (
+	stdinPtyFlag ptyIoFlags = 1 << iota
+	stdoutPtyFlag
+	stderrPtyFlag
+)
+
+type containerEntrySpec struct {
+	ptyPath string
+	ptyIo   ptyIoFlags
+	env     map[string]string
+
+	verbose bool
+	uid     int
+	cwd     string
+
+	command []string
+}
+
+type processExitType int
+
+const (
+	processExitNormal = iota
+	processExitSignaled
+)
+
+type processExitStatus struct {
+	exitType processExitType
+	// Either the exit code or the signal.
+	result int
+}
+
+// A "door" is responsible for entry into a container's environment.
+type containerDoor interface {
+	Enter(ct *container.Container, spec *containerEntrySpec) (*processExitStatus, error)
+}
 
 func getLeader(name string) (uint32, error) {
 	machined, err := machine1.New()
@@ -40,20 +73,6 @@ func getLeader(name string) (uint32, error) {
 	return props["Leader"].(uint32), nil
 }
 
-func fileIsTerminal(file *os.File) bool {
-	return terminal.IsTerminal(int(file.Fd()))
-}
-
-func anyFileIsTerminal(files []*os.File) bool {
-	for _, file := range files {
-		if fileIsTerminal(file) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func safeCopy(dest *os.File, source *os.File) {
 	hadSuccessYet := false
 
@@ -66,27 +85,37 @@ func safeCopy(dest *os.File, source *os.File) {
 				}
 			}
 
-			log.Fatalf("safeCopy %d -> %d failed: %T", source.Fd(), dest.Fd(), err)
+			log.Fatalf("safeCopy %d -> %d failed: %v", source.Fd(), dest.Fd(), err)
 		} else {
 			hadSuccessYet = true
 		}
 	}
 }
 
-func convertStateToExitCode(state *os.ProcessState) int {
-	// XXX: syscall is deprecated, but this cast will fail if it directly jumps to
-	// unix.WaitStatus.
-	waitStatus := unix.WaitStatus(state.Sys().(syscall.WaitStatus))
+func (spec containerEntrySpec) buildNsboxHostCommand() []string {
+	cmd := []string{"/run/host/nsbox/nsbox-host", "enter"}
 
-	if waitStatus.Signaled() {
-		// Treat a signal like most shells do, as 128 + the signal value.
-		return 128 + int(waitStatus.Signal())
-	} else if waitStatus.Exited() {
-		return waitStatus.ExitStatus()
-	} else {
-		log.Alertf("Unexpected wait status %d", int(waitStatus))
-		return 1
+	if spec.verbose {
+		cmd = append(cmd, "-v")
 	}
+
+	cmd = append(cmd, fmt.Sprintf("-uid=%d", spec.uid), fmt.Sprintf("-cwd=%s", spec.cwd))
+
+	// Add the -stdin,-stdout,-stderr=ptypath args if they are PTYs.
+	stdioFlags := map[string]ptyIoFlags{"stdin": stdinPtyFlag, "stdout": stdoutPtyFlag, "stderr": stderrPtyFlag}
+	for arg, ptyFlag := range stdioFlags {
+		if spec.ptyIo&ptyFlag != 0 {
+			cmd = append(cmd, fmt.Sprintf("-%s=%s", arg, spec.ptyPath))
+		}
+	}
+
+	// Add the environment variables.
+	cmd = append(cmd, "env")
+	for name, value := range spec.env {
+		cmd = append(cmd, fmt.Sprintf("%s=%s", name, value))
+	}
+
+	return append(cmd, spec.command...)
 }
 
 func EnterContainer(ct *container.Container, command []string, usrdata *userdata.Userdata, workdir string) (int, error) {
@@ -94,41 +123,59 @@ func EnterContainer(ct *container.Container, command []string, usrdata *userdata
 		command = []string{ct.Shell(usrdata), "-l"}
 	}
 
-	leader, err := getLeader(ct.Name)
-	if err != nil {
-		return 0, err
-	}
-
 	// XXX: Okay so PTY handling is a royal mess. nsenter doesn't give us a pty *at all*.
-	// Therefore, the plan of action is to ask machined for a pty inside the machine, and let
+	// Therefore, the plan of action is to ask the pty service for a pty inside the container, and let
 	// nsbox-enter know where to redirect what.
+
+	// However, if this is a booted container, the PTY will instead be set up using a systemd
+	// transient unit, which is necessary otherwise having an external process inside will
+	// mess up cgroups: https://ora.pm/project/211667/kanban/task/3069813
 
 	// Do not touch this code. If you do, and it breaks, you will be haunted by the ghost of
 	// several thousand headless terminals that were spawned and killed during the course of
 	// this code's development.
 
-	// NOTE: Order here is important, nsbox-enter.sh assumes it.
-
 	stdio := []*os.File{os.Stdin, os.Stdout, os.Stderr}
+	// Indexed using the corresponding FD.
+	stdioPtyFlags := []ptyIoFlags{stdinPtyFlag, stdoutPtyFlag, stderrPtyFlag}
 	var pty *os.File
+	var spec containerEntrySpec
 
 	var forwardStdinToPty = false
 	var forwardPtyToWriter *os.File
 
-	if anyFileIsTerminal(stdio) {
+	var door containerDoor
+	if ct.Config.Boot {
+		door = &systemdDoor{}
+	} else {
+		door = &nsenterDoor{}
+	}
+
+	for _, file := range stdio {
+		fd := int(file.Fd())
+		if terminal.IsTerminal(fd) {
+			spec.ptyIo |= stdioPtyFlags[fd]
+		}
+	}
+
+	if spec.ptyIo != 0 {
+		var err error
+		// Do NOT use := here, a new pty variable will be created and shadow the outer one.
 		pty, err = ptyservice.OpenPtyInContainer(ct)
 		if err != nil {
 			return 0, err
 		}
 
-		if fileIsTerminal(os.Stdin) {
+		spec.ptyPath = pty.Name()
+
+		if spec.ptyIo&stdinPtyFlag != 0 {
 			forwardStdinToPty = true
 		}
 
 		// If stdout isn't a tty, we can properly redirect stderr to stderr,
 		// but otherwise we can't really tell the difference.
-		if fileIsTerminal(os.Stdout) || fileIsTerminal(os.Stderr) {
-			if !fileIsTerminal(os.Stdout) {
+		if spec.ptyIo&(stdoutPtyFlag|stderrPtyFlag) != 0 {
+			if spec.ptyIo&stdoutPtyFlag == 0 {
 				forwardPtyToWriter = os.Stderr
 			} else {
 				forwardPtyToWriter = os.Stdout
@@ -136,106 +183,64 @@ func EnterContainer(ct *container.Container, command []string, usrdata *userdata
 		}
 	}
 
-	args := []string{"nsenter", "-at", strconv.Itoa(int(leader)), "/run/host/nsbox/nsbox-host", "enter"}
+	spec.uid = os.Getuid()
+	spec.cwd = workdir
+	spec.env = usrdata.Environ
+	spec.command = command
 
-	if log.Verbose() {
-		args = append(args, "-v")
-	}
-
-	ptyArgMap := []string{"stdin", "stdout", "stderr"}
-	for index, file := range stdio {
-		if !fileIsTerminal(file) {
-			continue
+	// Set-up the PTY forwarding.
+	if spec.ptyIo != 0 {
+		if forwardStdinToPty {
+			go safeCopy(pty, os.Stdin)
 		}
 
-		args = append(args, fmt.Sprintf("-%s=%s", ptyArgMap[index], pty.Name()))
-	}
+		if forwardPtyToWriter != nil {
+			go safeCopy(forwardPtyToWriter, pty)
+		}
 
-	args = append(args, fmt.Sprintf("-uid=%d", os.Getuid()), fmt.Sprintf("-cwd=%s", workdir))
-
-	// Add the environment variables.
-	args = append(args, "env")
-
-	for name, value := range usrdata.Environ {
-		args = append(args, fmt.Sprintf("%s=%s", name, value))
-	}
-
-	args = append(args, command...)
-
-	log.Debug("running:", args)
-
-	// If there's no pty, we can exec the command directly.
-	if pty == nil {
-		nsenter, err := exec.LookPath("nsenter")
+		oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
 		if err != nil {
-			return 0, errors.Wrap(err, "failed to find nsenter")
+			log.Debug("failed to make terminal raw", err)
+		} else {
+			defer terminal.Restore(int(os.Stdin.Fd()), oldState)
 		}
 
-		if err := unix.Exec(nsenter, args, os.Environ()); err != nil {
-			return 0, errors.Wrap(err, "failed to exec into namespace")
-		}
+		// Trace SIGWINCH to forward the window size.
+		sigchan := make(chan os.Signal)
+		signal.Notify(sigchan, unix.SIGWINCH)
 
-		panic("should not reach here")
-	}
+		defer func() {
+			signal.Stop(sigchan)
+			close(sigchan)
+		}()
 
-	// Forward the PTYs.
-	if forwardStdinToPty {
-		go safeCopy(pty, os.Stdin)
-	}
-
-	if forwardPtyToWriter != nil {
-		go safeCopy(forwardPtyToWriter, pty)
-	}
-
-	oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		log.Debug("failed to make terminal raw", err)
-	} else {
-		defer terminal.Restore(int(os.Stdin.Fd()), oldState)
-	}
-
-	// Trace SIGWINCH to forward the window size.
-	sigchan := make(chan os.Signal)
-	signal.Notify(sigchan, unix.SIGWINCH)
-
-	defer func() {
-		signal.Stop(sigchan)
-		close(sigchan)
-	}()
-
-	inheritSize := func() {
-		for _, file := range stdio {
-			if fileIsTerminal(file) {
-				krpty.InheritSize(file, pty)
-				break
+		inheritSize := func() {
+			for _, file := range stdio {
+				if spec.ptyIo&stdioPtyFlags[int(file.Fd())] != 0 {
+					krpty.InheritSize(file, pty)
+					break
+				}
 			}
 		}
+
+		inheritSize()
+
+		go func() {
+			for _ = range sigchan {
+				inheritSize()
+			}
+		}()
 	}
 
-	inheritSize()
-
-	go func() {
-		for _ = range sigchan {
-			inheritSize()
-		}
-	}()
-
-	if err := selinux.SetExecProcessContextContainer(); err != nil {
-		log.Alert("failed to set exec context to enter container:", err)
+	status, err := door.Enter(ct, &spec)
+	if err != nil {
+		return 0, err
 	}
 
-	// Finally run nsenter.
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	if err := cmd.Run(); err != nil {
-		// Handle ExitError later on.
-		if _, ok := err.(*exec.ExitError); !ok {
-			return 0, errors.Wrap(err, "failed to enter into namespace")
-		}
+	if status.exitType == processExitSignaled {
+		// Mimic the shell's exit code on signal.
+		return 128 + status.result, nil
+	} else {
+		return status.result, nil
 	}
-
-	return convertStateToExitCode(cmd.ProcessState), nil
 }

internal/session/enter_nsenter.go
@@ -1,0 +1,79 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package session
+
+import (
+	"os"
+	"os/exec"
+	"strconv"
+	"syscall"
+
+	"github.com/pkg/errors"
+	"github.com/refi64/nsbox/internal/container"
+	"github.com/refi64/nsbox/internal/log"
+	"github.com/refi64/nsbox/internal/selinux"
+	"golang.org/x/sys/unix"
+)
+
+// A door that enters the container environment via nsenter.
+type nsenterDoor struct{}
+
+func convertStateToProcessExit(state *os.ProcessState) (*processExitStatus, error) {
+	// XXX: syscall is deprecated, but this cast will fail if it directly jumps to
+	// unix.WaitStatus.
+	waitStatus := unix.WaitStatus(state.Sys().(syscall.WaitStatus))
+
+	if waitStatus.Signaled() {
+		return &processExitStatus{exitType: processExitSignaled, result: int(waitStatus.Signal())}, nil
+	} else if waitStatus.Exited() {
+		return &processExitStatus{exitType: processExitNormal, result: waitStatus.ExitStatus()}, nil
+	} else {
+		return nil, errors.Errorf("Unexpected wait status %d", int(waitStatus))
+	}
+}
+
+func (door *nsenterDoor) Enter(ct *container.Container, spec *containerEntrySpec) (*processExitStatus, error) {
+	leader, err := getLeader(ct.Name)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting leader process")
+	}
+
+	nsenterCmd := []string{"nsenter", "-at", strconv.Itoa(int(leader))}
+	nsenterCmd = append(nsenterCmd, spec.buildNsboxHostCommand()...)
+
+	log.Debug("running:", nsenterCmd)
+
+	if err := selinux.SetExecProcessContextContainer(); err != nil {
+		log.Alert("failed to set exec context to enter container:", err)
+	}
+
+	// If there's no pty, we can exec the command directly.
+	if spec.ptyPath == "" {
+		nsenter, err := exec.LookPath("nsenter")
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to find nsenter")
+		}
+
+		if err := unix.Exec(nsenter, nsenterCmd, os.Environ()); err != nil {
+			return nil, errors.Wrap(err, "failed to exec into namespace")
+		}
+
+		panic("should not reach here")
+	}
+
+	cmd := exec.Command(nsenterCmd[0], nsenterCmd[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		// Handle ExitError later on.
+		if _, ok := err.(*exec.ExitError); !ok {
+			return nil, errors.Wrap(err, "failed to enter into namespace")
+		}
+	}
+
+	return convertStateToProcessExit(cmd.ProcessState)
+}

commit 96365221d93a5dde0d28dbeacce7458808106563

daemon: Unbreak binding the Xorg socket
This was broken by 04d0099ada6d4550b3f64d2f0bd73e7119533a5f, since it
mounted a private /tmp which caused /tmp/.X11-unix to not be present.

internal/daemon/transient.go
@@ -17,10 +17,13 @@ import (
 	"github.com/pkg/errors"
 	"github.com/refi64/nsbox/internal/container"
 	"github.com/refi64/nsbox/internal/log"
+	"github.com/refi64/nsbox/internal/nsbus"
 	"github.com/refi64/nsbox/internal/paths"
 	"github.com/refi64/nsbox/internal/selinux"
 	"github.com/refi64/nsbox/internal/userdata"
 )
+
+type temporaryFileSystem struct{ Path, Options string }
 
 func startNsboxd(systemd *systemd1.Conn, nsboxd string, ct *container.Container, usrdata *userdata.Userdata) error {
 	serviceName := fmt.Sprintf("nsbox-%s.service", ct.Name)
@@ -67,8 +70,8 @@ func startNsboxd(systemd *systemd1.Conn, nsboxd string, ct *container.Container,
 		),
 		{
 			// This is needed for safety with use of nsbus, see there for more info.
-			Name:  "PrivateTmp",
-			Value: godbus.MakeVariant(true),
+			Name:  "TemporaryFileSystem",
+			Value: godbus.MakeVariant([]temporaryFileSystem{{Path: nsbus.PrivateBusTmpdir}}),
 		},
 		{
 			Name:  "Environment",

internal/nsbus/nsbus.go
@@ -24,14 +24,15 @@ import (
 	connecting to the D-Bus socket there. However, godbus's NewConn will not
 	give us a UNIX-style transport if we immediately create a connection from
 	the new fd, thus making passing of file descriptors broken. In order to
-	rectify this, a private (thanks to PrivateTmp=true in transient.go) D-Bus
+	rectify this, a private (thanks to mounting a tmpfs in transient.go) D-Bus
 	socket is created that just forwards everything to the true socket, and
 	the D-Bus connection is created by dialing into the forwarding socket
 	instead of the true one.
 */
 
+const PrivateBusTmpdir = "/tmp/nsbox"
+const privateBusPath = PrivateBusTmpdir + "/container_bus_socket"
 const busPath = "/run/dbus/system_bus_socket"
-const privateBusPath = "/tmp/nsbox_container_bus_socket"
 
 // Use an error value to propagate the fd up from inside the dialing.
 type dialSuccessError struct {