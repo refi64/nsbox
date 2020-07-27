299ee6c0f5238b4d9479da076d06a4cd2c2dd908
Ryan Gonzalez <rymg19@gmail.com>
2020-07-27 00:56:18 -0500

    build/parse_release: Allow setting a custom commit
    This makes it easy to determine what release # a commit was associated
    with.

--- build/parse_release.py
+++ build/parse_release.py
@@ -14,6 +14,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--root')
     parser.add_argument('--branch', choices=['stable', 'edge'])
+    parser.add_argument('--override-commit')
 
     args = parser.parse_args()
 
@@ -21,28 +22,42 @@ def main():
     if args.branch == 'edge':
         fmt += '.%h'
 
-    version_proc = subprocess.run(['git', '-C', args.root, 'log', '-1', f'--format={fmt}'],
-                                  stdout=subprocess.PIPE, check=True, universal_newlines=True,
+    git_version_cmd = ['git', '-C', args.root, 'log', '-1', f'--format={fmt}']
+    if args.override_commit is not None:
+        git_version_cmd.append(args.override_commit)
+
+    version_proc = subprocess.run(git_version_cmd,
+                                  stdout=subprocess.PIPE,
+                                  check=True,
+                                  universal_newlines=True,
                                   cwd=args.root)
     version_proc_parts = version_proc.stdout.strip().split('.')
     assert len(version_proc_parts) in (1, 2), version_proc_parts
 
     data = {
-        'version': time.strftime('%y.%m.%d', time.gmtime(int(version_proc_parts[0]))),
-        'commit': '',
+        'version':
+        time.strftime('%y.%m.%d', time.gmtime(int(version_proc_parts[0]))),
+        'commit':
+        '',
     }
 
     if args.branch == 'edge':
         data['commit'] = version_proc_parts[1]
 
-        rev_count_proc = subprocess.run(['git', 'rev-list', '--count', 'HEAD'],
-                                        stdout=subprocess.PIPE, check=True,
-                                        universal_newlines=True, cwd=args.root)
+        git_rev_list_cmd = [
+            'git', 'rev-list', '--count', args.override_commit or 'HEAD'
+        ]
+        rev_count_proc = subprocess.run(git_rev_list_cmd,
+                                        stdout=subprocess.PIPE,
+                                        check=True,
+                                        universal_newlines=True,
+                                        cwd=args.root)
         rev_count = rev_count_proc.stdout.strip()
 
         data['version'] += f'.{rev_count}'
 
     print(json.dumps(data))
+
 
 if __name__ == '__main__':
     main()
